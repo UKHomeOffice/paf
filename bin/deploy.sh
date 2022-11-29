f4a3aefc78d6e2357e384a2b3c2ea4cd114861ad
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2022-11-29 18:23:15 +0000

    testing drone

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 set -e
+set -x
 
 export INGRESS_INTERNAL_ANNOTATIONS=$HOF_CONFIG/ingress-internal-annotations.yaml
 export INGRESS_EXTERNAL_ANNOTATIONS=$HOF_CONFIG/ingress-external-annotations.yaml
 export CONFIGMAP_VALUES=$HOF_CONFIG/configmap-values.yaml
 export NGINX_SETTINGS=$HOF_CONFIG/nginx-settings.yaml
-export DATA_SERVICE_EXTERNAL_ANNOTATIONS=$HOF_CONFIG/data-service-external-annotations.yaml
-
-export SCHEMA_ACTION=migrate
 
 kd='kd --insecure-skip-tls-verify --timeout 10m --check-interval 10s'
 
@@ -15,10 +13,9 @@ if [[ $1 == 'tear_down' ]]; then
   export KUBE_NAMESPACE=$BRANCH_ENV
   export DRONE_SOURCE_BRANCH=$(cat /root/.dockersock/branch_name.txt)
 
-  $kd --delete -f kube/jobs/ms-schema-job.yml
   $kd --delete -f kube/configmaps/configmap.yml
-  $kd --delete -f kube/redis -f kube/save-return-data -f kube/clue-resolver  -f kube/html-pdf -f kube/app
-  echo "Torn Down UAT Branch - paf-$DRONE_SOURCE_BRANCH.internal.paf-branch.homeoffice.gov.uk"
+  $kd --delete -f kube/redis -f kube/app
+  echo "Torn Down UAT Branch - paf-$DRONE_SOURCE_BRANCH.internal.$BRANCH_ENV.homeoffice.gov.uk"
   exit 0
 fi
 
@@ -26,42 +23,26 @@ export KUBE_NAMESPACE=$1
 export DRONE_SOURCE_BRANCH=$(echo $DRONE_SOURCE_BRANCH | tr '[:upper:]' '[:lower:]' | tr '/' '-')
 
 if [[ ${KUBE_NAMESPACE} == ${BRANCH_ENV} ]]; then
-  $kd --delete -f kube/jobs/ms-schema-job.yml
-  $kd -f kube/jobs/ms-schema-job.yml
-  $kd -f kube/configmaps -f kube/certs
-  # $kd -f kube/clue-resolver
-  $kd -f kube/html-pdf -f kube/save-return-data
-  $kd -f kube/redis
+  $kd -f kube/configmaps 
+  $kd -f kube/certs
+  $kd -f kube/redis 
   $kd -f kube/app
 elif [[ ${KUBE_NAMESPACE} == ${UAT_ENV} ]]; then
-  $kd --delete -f kube/jobs/ms-schema-job.yml
-  $kd -f kube/jobs/ms-schema-job.yml
-  $kd -f kube/configmaps/configmap.yml
-  # $kd -f kube/clue-resolver
-  $kd -f kube/html-pdf -f kube/save-return-data
-  $kd -f kube/redis
-  $kd -f kube/app
+  $kd -f kube/configmaps/configmap.yml -f kube/app/service.yml
+  $kd -f kube/app/networkpolicy-internal.yml -f kube/app/ingress-internal.yml
+  $kd -f kube/redis -f kube/app/deployment.yml
 elif [[ ${KUBE_NAMESPACE} == ${STG_ENV} ]]; then
-  $kd --delete -f kube/jobs/ms-schema-job.yml
-  $kd -f kube/jobs/ms-schema-job.yml
-  $kd -f kube/configmaps/configmap.yml
-  $kd -f kube/clue-resolver -f kube/html-pdf
-  $kd -f kube/app/ingress-internal.yml -f kube/app/networkpolicy-internal.yml
-  $kd -f kube/redis -f kube/save-return-data
-  $kd -f kube/app
+  $kd -f kube/configmaps/configmap.yml -f kube/app/service.yml
+  $kd -f kube/app/networkpolicy-internal.yml -f kube/app/ingress-internal.yml
+  $kd -f kube/redis -f kube/app/deployment.yml
 elif [[ ${KUBE_NAMESPACE} == ${PROD_ENV} ]]; then
-  $kd --delete -f kube/jobs/ms-schema-job.yml
-  $kd -f kube/jobs/ms-schema-job.yml
-  $kd -f kube/configmaps/configmap.yml  -f kube/app/service.yml
-  $kd -f kube/clue-resolver -f kube/html-pdf
-  $kd -f kube/app/ingress-external.yml -f kube/app/networkpolicy-external.yml
-  $kd -f kube/redis -f kube/save-return-data
-  $kd -f kube/app/deployment.yml
+  $kd -f kube/configmaps/configmap.yml -f kube/app/service.yml
+  $kd -f kube/app/networkpolicy-external.yml -f kube/app/ingress-external.yml
+  $kd -f kube/redis -f kube/app/deployment.yml
 fi
 
 sleep $READY_FOR_TEST_DELAY
 
 if [[ ${KUBE_NAMESPACE} == ${BRANCH_ENV} ]]; then
-  echo "App Branch - paf-$DRONE_SOURCE_BRANCH.internal.paf-branch.homeoffice.gov.uk"
-  echo "Data Service Branch - data-service-$DRONE_SOURCE_BRANCH.paf-branch.homeoffice.gov.uk"
+  echo "UAT Branch - paf-$DRONE_SOURCE_BRANCH.internal.$BRANCH_ENV.homeoffice.gov.uk"
 fi
