d4b3f70ae5f0249bd346eb1ea417c941edf14ec1
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2022-12-14 19:37:12 +0000

    PAF-25: create initial unit tests

---
+++ test/_unit/server.spec.js
@@ -0,0 +1,120 @@
+
+const path = require('path');
+
+describe('Server.js app file', () => {
+  let hofStub;
+  let useStub;
+  let sendStub;
+  let appsPafStub;
+  let req;
+  let res;
+  let next;
+
+  beforeEach(() => {
+    req = request();
+    req.session = {};
+    req.body = {
+      appName: 'testApp',
+      sessionProperties: {
+        testProp1: 'test',
+        testProp2: 'test'
+      }
+    };
+
+    res = response();
+    sendStub = sinon.stub();
+    res.send = sendStub;
+    next = sinon.stub();
+    hofStub = sinon.stub();
+    useStub = sinon.stub();
+    appsPafStub = sinon.stub();
+
+    useStub.onCall(0).yields(req, res, next);
+    useStub.onCall(1).yields(req, res);
+    hofStub.returns({ use: useStub });
+
+    proxyquire('../server', {
+      hof: hofStub,
+      './apps/paf': appsPafStub,
+      './config': { env: 'test' }
+    });
+  });
+
+  describe('Setup HOF Configuration', () => {
+    it('calls hof with routes', () => {
+      hofStub.should.have.been.calledOnce.calledWithExactly({
+        appName: 'Public Allegations Form',
+        theme: 'govUK',
+         routes: [
+          appsPafStub
+        ],
+        session: { name: 'paf.hof.sid' }
+      });
+    });
+
+    it('should call the app use method twice if env set to test', () => {
+      useStub.callCount.should.equal(2);
+    });
+
+    it('should call the app use method twice if env set to development', () => {
+      const use = sinon.stub();
+      const hof = () => ({ use });
+
+      proxyquire('../server', {
+        hof: hof,
+        './config': { env: 'development' }
+      });
+
+      useStub.callCount.should.equal(2);
+    });
+
+    it('should call the app use method once if env set to anything else', () => {
+      const use = sinon.stub();
+      const hof = () => ({ use });
+
+      proxyquire('../server', {
+        hof: hof,
+        './config': { env: 'production' }
+      });
+
+      use.should.have.been.calledOnce;
+    });
+  });
+
+  describe('Use Locals', () => {
+    it('should set locals on the response', () => {
+      res.locals.should.eql({
+        htmlLang: 'en',
+        feedbackUrl: 'https://eforms.homeoffice.gov.uk/outreach/feedback.ofml',
+      });
+    });
+
+    it('should call next once', () => {
+      next.should.have.been.calledOnce;
+    });
+  });
+
+  describe('Use Test Endpoint', () => {
+    it('it should take /test/bootstrap-session as the first argument', () => {
+      const useArgs = useStub.getCall(1).args[0];
+      useArgs.should.eql('/test/bootstrap-session');
+    });
+
+    it('the send method on res should be called', () => {
+      sendStub.should.have.been.calledOnce.calledWithExactly('Session populate complete');
+    });
+
+    it('if no app name key set but a redis session is available set the app name key to an empty object', () => {
+      expect(req.session['hof-wizard-testApp']).to.exist;
+    });
+
+    it('if session properties are set in the body they are set on hof-wizard-appName', () => {
+      req.session['hof-wizard-testApp'].should.eql(
+        {
+          testProp1: 'test',
+          testProp2: 'test'
+        }
+      );
+    });
+  });
+});
