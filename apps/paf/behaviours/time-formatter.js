2316b17627f93bcb3ce124dd4a2e265b558de33e
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2023-04-26 13:28:45 +0100

    add styling to time fields and add change link to time field on check answers page

--- apps/paf/behaviours/time-formatter.js
+++ apps/paf/behaviours/time-formatter.js
@@ -1,8 +1,11 @@
+const _ = require('lodash');
+
 module.exports = superclass => class extends superclass {
   configure(req, res, next) {
 
     // preprend '0' if number is only a single digit
     const pad = num => num !== '' && num.length < 2 ? `0${num}` : num;
+
     if (req.sessionModel.get('time-crime-will-happen-hour')) {
       req.sessionModel.set('time-crime-will-happen-hour', pad(req.sessionModel.get('time-crime-will-happen-hour')))
     }
@@ -22,5 +25,28 @@ module.exports = superclass => class extends superclass {
     }
 
     return super.configure(req, res, next);
+  }
+
+  locals(req, res) {
+    const locals = super.locals(req, res);
+    // set change link for for time-crime-will-happen field
+    if (locals.route === 'confirm') {
+      _.forEach(locals.rows, (fields) => {
+        locals.rows = locals.rows.map(row => {
+          if (row.section === 'The Crime - Duration') {
+            _.forEach(fields, (sectionFields) => {
+              _.forEach(sectionFields, (field) => {
+                if (field.field === 'time-crime-will-happen') {
+                  field.changeLink = '/paf/date-time-crime-will-happen/edit#time-crime-will-happen-hour'
+                }
+              })
+            })
+            return row;
+          }
+          return row
+        });
+      })
+    }
+    return locals;
   }
 };
