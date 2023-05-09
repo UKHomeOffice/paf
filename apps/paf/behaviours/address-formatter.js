bfd8e31df1cc908457d4fb39202972f6d3d36cdd
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2023-05-09 12:43:04 +0100

    clean up address formatter

--- apps/paf/behaviours/address-formatter.js
+++ apps/paf/behaviours/address-formatter.js
@@ -1,12 +1,13 @@
 'use strict';
 
 
-module.exports = superclass => class SendToSQS extends superclass {
+module.exports = superclass => class extends superclass {
   configure(req, res, next) {
     if (req.sessionModel.get('crime-location-address-line1') || req.sessionModel.get('crime-location-address-line2') || req.sessionModel.get('crime-location-address-town') || req.sessionModel.get('crime-location-address-county')) {
       let crimeAddress = [];
       crimeAddress.push(req.sessionModel.get('crime-location-address-line1'), req.sessionModel.get('crime-location-address-line2'), req.sessionModel.get('crime-location-address-town'), req.sessionModel.get('crime-location-address-county'))
-      req.sessionModel.set('crime-location-address', crimeAddress.toString());
+      crimeAddress = crimeAddress.filter((str) => str !== '')
+      req.sessionModel.set('crime-location-address', crimeAddress.join(', '));
     } else {
       req.sessionModel.unset('crime-location-address')
     }
@@ -14,7 +15,8 @@ module.exports = superclass => class SendToSQS extends superclass {
     if (req.sessionModel.get('crime-another-location-address-line1') || req.sessionModel.get('crime-another-location-address-line2') || req.sessionModel.get('crime-another-location-address-town') || req.sessionModel.get('crime-another-location-address-county')) {
       let crimeAnotherAddress = [];
       crimeAnotherAddress.push(req.sessionModel.get('crime-another-location-address-line1'), req.sessionModel.get('crime-another-location-address-line2'), req.sessionModel.get('crime-another-location-address-town'), req.sessionModel.get('crime-another-location-address-county'))
-      req.sessionModel.set('crime-another-location-address', crimeAnotherAddress.toString());
+      crimeAnotherAddress = crimeAnotherAddress.filter((str) => str !== '')
+      req.sessionModel.set('crime-another-location-address', crimeAnotherAddress.join(', '));
     } else {
       req.sessionModel.unset('crime-another-location-address')
     }
@@ -22,7 +24,8 @@ module.exports = superclass => class SendToSQS extends superclass {
     if (req.sessionModel.get('report-person-location-uk-address-line1') || req.sessionModel.get('report-person-location-uk-address-line2') || req.sessionModel.get('report-person-location-uk-address-town') || req.sessionModel.get('report-person-location-uk-address-county')) {
       let personUkAddress = [];
       personUkAddress.push(req.sessionModel.get('report-person-location-uk-address-line1'), req.sessionModel.get('report-person-location-uk-address-line2'), req.sessionModel.get('report-person-location-uk-address-town'), req.sessionModel.get('report-person-location-uk-address-county'))
-      req.sessionModel.set('report-person-location-uk-address', personUkAddress.toString());
+      personUkAddress = personUkAddress.filter((str) => str !== '')
+      req.sessionModel.set('report-person-location-uk-address', personUkAddress.join(', '));
     } else {
       req.sessionModel.unset('report-person-location-uk-address')
     }
@@ -30,7 +33,8 @@ module.exports = superclass => class SendToSQS extends superclass {
     if (req.sessionModel.get('report-person-location-outside-uk-address-line1') || req.sessionModel.get('report-person-location-outside-uk-address-line2') || req.sessionModel.get('report-person-location-outside-uk-address-town') || req.sessionModel.get('report-person-location-outside-uk-address-county')) {
       let personOutsideUkAddress = [];
       personOutsideUkAddress.push(req.sessionModel.get('report-person-location-outside-uk-address-line1'), req.sessionModel.get('report-person-location-outside-uk-address-line2'), req.sessionModel.get('report-person-location-outside-uk-address-town'), req.sessionModel.get('report-person-location-outside-uk-address-county'))
-      req.sessionModel.set('report-person-location-outside-uk-address', personOutsideUkAddress.toString());
+      personOutsideUkAddress = personOutsideUkAddress.filter((str) => str !== '')
+      req.sessionModel.set('report-person-location-outside-uk-address', personOutsideUkAddress.join(', '));
     } else {
       req.sessionModel.unset('report-person-location-outside-uk-address')
     }
@@ -38,7 +42,8 @@ module.exports = superclass => class SendToSQS extends superclass {
     if (req.sessionModel.get('report-person-occupation-company-address-line1') || req.sessionModel.get('report-person-occupation-company-address-line2') || req.sessionModel.get('report-person-occupation-company-address-town') || req.sessionModel.get('report-person-occupation-company-address-county')) {
       let personWorkAddress = [];
       personWorkAddress.push(req.sessionModel.get('report-person-occupation-company-address-line1'), req.sessionModel.get('report-person-occupation-company-address-line2'), req.sessionModel.get('report-person-occupation-company-address-town'), req.sessionModel.get('report-person-occupation-company-address-county'))
-      req.sessionModel.set('report-person-occupation-company-address', personWorkAddress.toString());
+      personWorkAddress = personWorkAddress.filter((str) => str !== '')
+      req.sessionModel.set('report-person-occupation-company-address', personWorkAddress.join(', '));
     } else {
       req.sessionModel.unset('report-person-occupation-company-address')
     }
@@ -46,7 +51,8 @@ module.exports = superclass => class SendToSQS extends superclass {
     if (req.sessionModel.get('report-person-study-address-line1') || req.sessionModel.get('report-person-study-address-line2') || req.sessionModel.get('report-person-study-address-town') || req.sessionModel.get('report-person-study-address-county')) {
       let personStudyAddress = [];
       personStudyAddress.push(req.sessionModel.get('report-person-study-address-line1'), req.sessionModel.get('report-person-study-address-line2'), req.sessionModel.get('report-person-study-address-town'), req.sessionModel.get('report-person-study-address-county'))
-      req.sessionModel.set('report-person-study-address', personStudyAddress.toString());
+      personStudyAddress = personStudyAddress.filter((str) => str !== '')
+      req.sessionModel.set('report-person-study-address', personStudyAddress.join(', '));
     } else {
       req.sessionModel.unset('report-person-study-address')
     }
@@ -54,7 +60,8 @@ module.exports = superclass => class SendToSQS extends superclass {
     if (req.sessionModel.get('company-address-line1') || req.sessionModel.get('company-address-line2') || req.sessionModel.get('company-town') || req.sessionModel.get('company-county')) {
       let companyAddress = [];
       companyAddress.push(req.sessionModel.get('company-address-line1'), req.sessionModel.get('company-address-line2'), req.sessionModel.get('company-town'), req.sessionModel.get('company-county'))
-      req.sessionModel.set('company-address', companyAddress.toString());
+      companyAddress = companyAddress.filter((str) => str !== '')
+      req.sessionModel.set('company-address', companyAddress.join(', '));
     } else {
       req.sessionModel.unset('company-address')
     }
