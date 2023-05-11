7b41465fd93544a3d581719dbfc9f0d1d69427fe
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2023-05-11 18:25:39 +0100

    amend additional person formatter

--- apps/paf/behaviours/additional-person-formatter.js
+++ apps/paf/behaviours/additional-person-formatter.js
@@ -3,13 +3,17 @@ module.exports = superclass => class  extends superclass {
   configure(req, res, next) {
     const persons = req.sessionModel.get('persons')
     if (persons) {
+      let additionalPeople = [];
+
       _.forEach(persons.aggregatedValues, (i) => {
-        const additionalPeople = i.fields.map(item => (item.value));
+        const peopleValue = i.fields.map(item => (item.value));
         
         // Remove person number value as this is not relevant for IMS
-        additionalPeople.splice(0,1);
+        peopleValue.splice(0,1);
 
-        req.sessionModel.set('additional-people-table', 'Person Added IS =' + additionalPeople.join(', '));
+        additionalPeople.push('Person Added IS =' + peopleValue.join(', '))
+
+        req.sessionModel.set('additional-people-table', additionalPeople);
       })
     } 
     else {
