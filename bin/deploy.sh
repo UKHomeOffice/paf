4a386886527566afd0c97c91051dd198c650a98c
james.hovell <james.hovell@digital.homeoffice.gov.uk>
2023-05-17 12:14:39 +0100

    added external ingress for poise access

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -34,6 +34,7 @@ if [[ ${KUBE_NAMESPACE} == ${BRANCH_ENV} ]]; then
 elif [[ ${KUBE_NAMESPACE} == ${UAT_ENV} ]]; then
   $kd -f kube/configmaps/configmap.yml -f kube/app/service.yml
   $kd -f kube/app/networkpolicy-internal.yml -f kube/app/ingress-internal.yml
+  $kd -f kube/app/networkpolicy-external.yml -f kube/app/ingress-external.yml
   $kd -f kube/redis -f kube/app/deployment.yml
   $kd -f kube/ims-resolver
 elif [[ ${KUBE_NAMESPACE} == ${STG_ENV} ]]; then
