a4ae48043945e430f80c29eeb58bdffb56cc6d47
shamiluwais <mohamed.uwais@digital.homeoffice.gov.uk>
2023-05-25 17:25:54 +0100

    Config amendments
    - Removed endpoint config used for local stack
    - changed format of aws credentials
    - set aws region

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 'use strict';
 /* eslint no-process-env: 0 */
-const AWS = require('aws-sdk');
 
 const env = process.env.NODE_ENV;
 const useMocks = process.env.USE_MOCKS === 'true' || !env;
@@ -27,10 +26,11 @@ module.exports = {
     secret: process.env.KEYCLOAK_SECRET
   },
   awsSqs: {
-    endpoint: new AWS.Endpoint('http://localhost:4566'), // remove later
-    region: process.env.AWS_REGION,
+    region: process.env.AWS_REGION || 'eu-west-2',
     queueUrl: process.env.SQS_URL,
-    accessKeyId: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY
+    credentials: {
+      accessKeyId: process.env.ACCESS_KEY_ID,
+      secretAccessKey: process.env.SECRET_ACCESS_KEY
+    }
   }
 };
