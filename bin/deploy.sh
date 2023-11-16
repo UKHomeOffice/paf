81994a6e7b07caeda20e95ab76a64e94a9a83a13
SavantDhindsaHO <savantsingh.dhindsa@digital.homeoffice.gov.uk>
2023-11-16 16:21:15 +0000

    PAF-100 Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -33,6 +33,7 @@ if [[ ${KUBE_NAMESPACE} == ${BRANCH_ENV} ]]; then
   $kd -f kube/file-vault
 elif [[ ${KUBE_NAMESPACE} == ${UAT_ENV} ]]; then
   $kd -f kube/configmaps/configmap.yml -f kube/app/service.yml
+  $kd -f kube/certs
   $kd -f kube/app/networkpolicy-internal.yml -f kube/app/ingress-internal.yml
   $kd -f kube/app/networkpolicy-external.yml -f kube/app/ingress-external.yml
   $kd -f kube/redis -f kube/app/deployment.yml
