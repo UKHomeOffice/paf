b0f429828ec3c20095423a9404cd1c58d47934e9
SavantDhindsaHO <savantsingh.dhindsa@digital.homeoffice.gov.uk>
2023-11-17 10:44:54 +0000

    PAF-100 Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -45,9 +45,11 @@ elif [[ ${KUBE_NAMESPACE} == ${STG_ENV} ]]; then
   $kd -f kube/redis -f kube/app/deployment.yml
   $kd -f kube/ims-resolver
 elif [[ ${KUBE_NAMESPACE} == ${PROD_ENV} ]]; then
+  $kd -f kube/file-vault/file-vault-ingress.yml
   $kd -f kube/configmaps/configmap.yml -f kube/app/service.yml
+  $kd -f kube/certs
   $kd -f kube/app/networkpolicy-external.yml -f kube/app/ingress-external.yml
-  $kd -f kube/redis -f kube/app/deployment.yml
+  $kd -f kube/redis -f kube/file-vault -f kube/app/deployment.yml
   $kd -f kube/ims-resolver
 fi
 
