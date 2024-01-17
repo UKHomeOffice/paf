87a63f86328d651b15d0715e9cf113d541a05582
muthrajan <mylrajan.muthusamy1@digital.homeoffice.gov.uk>
2024-01-17 18:32:20 +0000

    PAF-179 request entity too large error bug fix

--- config.js
+++ config.js
@@ -13,6 +13,32 @@ module.exports = {
   useMocks: useMocks,
   upload: {
     maxFileSize: '100mb',
+    allowedMimeTypes: [
+      'application/json',
+      'application/msword',
+      'application/pdf',
+      'application/rtf',
+      'application/vnd.ms-excel',
+      'application/vnd.ms-outlook',
+      'application/vnd.ms-powerpoint',
+      'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.template',
+      'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+      'application/xml',
+      'application/x-tika-ooxml',
+      'audio/vnd.wave',
+      'audio/wav',
+      'audio/x-wav',
+      'image/bmp',
+      'image/jpeg',
+      'image/jpg',
+      'image/png',
+      'message/rfc822',
+      'text/csv',
+      'text/plain',
+      'text/xml'
+    ],
     // if mocks set use file service served up by app otherwise use filevault's port 3000
     hostname: !useMocks && process.env.FILE_VAULT_URL ?
       process.env.FILE_VAULT_URL :
