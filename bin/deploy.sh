f62a54070825eb579c880d254eebc3ecf529994f
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2024-01-19 15:20:05 +0000

    Fix merge conflicts part two

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -36,7 +36,8 @@ elif [[ ${KUBE_NAMESPACE} == ${UAT_ENV} ]]; then
   $kd -f kube/configmaps/configmap.yml -f kube/app/service.yml
   $kd -f kube/certs
   $kd -f kube/app/networkpolicy-internal.yml -f kube/app/ingress-internal.yml
-  $kd -f kube/redis -f kube/app/deployment.yml
+  $kd -f kube/app/networkpolicy-external.yml -f kube/app/ingress-external.yml
+  $kd -f kube/redis -f kube/file-vault -f kube/app/deployment.yml
   $kd -f kube/ims-resolver
 elif [[ ${KUBE_NAMESPACE} == ${STG_ENV} ]]; then
   $kd -f kube/configmaps/configmap.yml -f kube/app/service.yml
@@ -48,7 +49,7 @@ elif [[ ${KUBE_NAMESPACE} == ${PROD_ENV} ]]; then
   $kd -f kube/configmaps/configmap.yml -f kube/app/service.yml
   $kd -f kube/certs
   $kd -f kube/app/networkpolicy-external.yml -f kube/app/ingress-external.yml
-  $kd -f kube/redis -f kube/app/deployment.yml
+  $kd -f kube/redis -f kube/file-vault -f kube/app/deployment.yml
   $kd -f kube/ims-resolver
 fi
 
