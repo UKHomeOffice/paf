12b8eee8fbc23cf48e169a7d05bdfa4d86252921
JHoldergov <125476151+JHoldergov@users.noreply.github.com>
2024-02-01 11:20:45 +0000

    adding the cert mount on the bash file

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -27,6 +27,7 @@ export DRONE_SOURCE_BRANCH=$(echo $DRONE_SOURCE_BRANCH | tr '[:upper:]' '[:lower
 if [[ ${KUBE_NAMESPACE} == ${BRANCH_ENV} ]]; then
   $kd -f kube/file-vault/file-vault-ingress.yml
   $kd -f kube/configmaps
+  $kd -f kube/certmounts
   $kd -f kube/certs
   $kd -f kube/redis
   $kd -f kube/app
@@ -35,6 +36,7 @@ if [[ ${KUBE_NAMESPACE} == ${BRANCH_ENV} ]]; then
 elif [[ ${KUBE_NAMESPACE} == ${UAT_ENV} ]]; then
   $kd -f kube/file-vault/file-vault-ingress.yml
   $kd -f kube/configmaps/configmap.yml -f kube/app/service.yml
+  $kd -f kube/certmounts
   $kd -f kube/certs
   $kd -f kube/app/networkpolicy-internal.yml -f kube/app/ingress-internal.yml
   $kd -f kube/app/networkpolicy-external.yml -f kube/app/ingress-external.yml
@@ -48,6 +50,7 @@ elif [[ ${KUBE_NAMESPACE} == ${STG_ENV} ]]; then
 elif [[ ${KUBE_NAMESPACE} == ${PROD_ENV} ]]; then
   $kd -f kube/file-vault/file-vault-ingress.yml
   $kd -f kube/configmaps/configmap.yml -f kube/app/service.yml
+  $kd -f kube/certmounts
   $kd -f kube/certs
   $kd -f kube/app/networkpolicy-external.yml -f kube/app/ingress-external.yml
   $kd -f kube/redis -f kube/file-vault -f kube/app/deployment.yml
