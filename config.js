82713c3b40b0fca70efe9bf32b5fc271dcfe786a
JHoldergov <125476151+JHoldergov@users.noreply.github.com>
2024-02-01 10:30:02 +0000

    rebase

--- config.js
+++ config.js
@@ -3,7 +3,6 @@
 
 const env = process.env.NODE_ENV;
 const useMocks = process.env.USE_MOCKS === 'true' || !env;
-const useMocks = process.env.USE_MOCKS === 'true' || !env;
 
 module.exports = {
   env: env,
@@ -14,6 +13,19 @@ module.exports = {
   useMocks: useMocks,
   upload: {
     maxFileSize: '100mb',
+    allowedMimeTypes: [
+      'application/pdf',
+      'application/msword',
+      'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+      'application/vnd.ms-excel',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      'image/jpeg',
+      'image/jpg',
+      'image/png',
+      'text/plain',
+      'application/vnd.oasis.opendocument.text',
+      'application/vnd.oasis.opendocument.spreadsheet'
+    ],
     // if mocks set use file service served up by app otherwise use filevault's port 3000
     hostname: !useMocks && process.env.FILE_VAULT_URL ?
       process.env.FILE_VAULT_URL :
@@ -26,4 +38,12 @@ module.exports = {
     clientId: process.env.KEYCLOAK_CLIENT_ID,
     secret: process.env.KEYCLOAK_SECRET
   },
+  awsSqs: {
+    region: process.env.AWS_REGION || 'eu-west-2',
+    queueUrl: process.env.SQS_URL,
+    credentials: {
+      accessKeyId: process.env.ACCESS_KEY_ID,
+      secretAccessKey: process.env.SECRET_ACCESS_KEY
+    }
+  }
 };
