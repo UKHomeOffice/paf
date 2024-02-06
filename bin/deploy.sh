31863db5c7df6274f65c062d24695472c38c62a4
JHoldergov <125476151+JHoldergov@users.noreply.github.com>
2024-02-06 12:04:26 +0000

    env_var name

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,7 +4,7 @@ set -e
 export INGRESS_INTERNAL_ANNOTATIONS=$HOF_CONFIG/ingress-internal-annotations.yaml
 export INGRESS_EXTERNAL_ANNOTATIONS=$HOF_CONFIG/ingress-external-annotations.yaml
 export CONFIGMAP_VALUES=$HOF_CONFIG/configmap-values.yaml
-export CONFIGMAP1_VALUES=$HOF_CONFIG/imscertchain-store.yaml
+export CERTCONFIGMAP_VALUES=$HOF_CONFIG/imscertchain-store.yaml
 export NGINX_SETTINGS=$HOF_CONFIG/nginx-settings.yaml
 export FILEVAULT_NGINX_SETTINGS=$HOF_CONFIG/filevault-nginx-settings.yaml
 export FILEVAULT_INGRESS_EXTERNAL_ANNOTATIONS=$HOF_CONFIG/filevault-ingress-external-annotations.yaml
