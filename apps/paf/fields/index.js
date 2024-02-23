ccb9919c886e52916bd8e2a2600015f668b989b7
TemitopeAyokuHO <temitope.ayoku@digital.homeoffice.gov.uk>
2024-02-23 10:02:59 +0000

    fix bug in fields folder

--- apps/paf/fields/index.js
+++ apps/paf/fields/index.js
@@ -663,7 +663,11 @@ module.exports = {
   },
   'crime-another-location-phone': {
     className: ['govuk-input', 'govuk-input--width-20'],
-    validate: ['required', 'internationalPhoneNumber', { type: 'maxlength', arguments: 20 }]
+    validate: ['required', 'internationalPhoneNumber', { type: 'maxlength', arguments: 20 }],
+    dependent: {
+      field: 'crime-another-location',
+      value: 'yes'
+    }
   },
   'report-person': {
     mixin: 'radio-group',
