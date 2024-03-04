fb154cc1b5d8d4eabbd43fee3a4cb524b9347209
shamiluwais <mohamed.uwais@digital.homeoffice.gov.uk>
2024-03-04 16:14:49 +0000

    correct prod url

--- apps/paf/fields/index.js
+++ apps/paf/fields/index.js
@@ -1241,9 +1241,9 @@ module.exports = {
     mixin: 'radio-group',
     isPageHeading: true,
     options: [
-      'Male',
-      'Female',
-      'Other',
+      'male',
+      'female',
+      'other',
       'add-gender-unknown'
     ]
   },
