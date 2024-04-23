71cdb957f8dcd48b00998067337cd5f9a637b373
shamiluwais <mohamed.uwais@digital.homeoffice.gov.uk>
2024-04-23 12:33:22 +0100

    add certs back

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -53,6 +53,7 @@ elif [[ ${KUBE_NAMESPACE} == ${PROD_ENV} ]]; then
   $kd -f kube/configmaps/configmap.yml  -f kube/app/service.yml
   $kd -f kube/file-vault/file-vault-ingress.yml
   $kd -f kube/app/ingress-external.yml -f kube/app/networkpolicy-external.yml
+  $kd -f kube/certmounts/certmounts-prod.yml
   $kd -f kube/redis -f kube/file-vault
   $kd -f kube/app/deployment.yml
   $kd -f kube/ims-resolver
