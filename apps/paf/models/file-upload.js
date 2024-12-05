a160e39358396f3ab91cefdc468713421299aa1c
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2024-12-05 09:23:33 +0000

    logs

--- apps/paf/models/file-upload.js
+++ apps/paf/models/file-upload.js
@@ -32,7 +32,7 @@ module.exports = class UploadModel extends Model {
       };
       reqConf.method = 'POST';
       const result = await this.request(reqConf);
-      console.log('****************************RESULT, ', result);
+      console.log('****************************reqConf, ', reqConf);
 
       this.set({ url: result.url.replace('/file/', '/file/generate-link/').split('?')[0] });
       logger.info(`Successfully saved data`);
