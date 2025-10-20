19e4d87bbbaceed352f382d594605d7728c97198
adityababumallisettiHO <146218064+adityababumallisettiHO@users.noreply.github.com>
2025-10-20 19:20:20 +0100

    Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -47,8 +47,12 @@ elif [[ ${KUBE_NAMESPACE} == ${UAT_ENV} ]]; then
   $kd -f kube/ims-resolver
 elif [[ ${KUBE_NAMESPACE} == ${STG_ENV} ]]; then
   $kd -f kube/configmaps/configmap.yml -f kube/app/service.yml
+  $kd -f kube/certmounts/certmounts.yml
+  $kd -f kube/file-vault/file-vault-ingress.yml
+  $kd -f kube/app/ingress-external.yml -f kube/app/networkpolicy-external.yml
   $kd -f kube/app/networkpolicy-internal.yml -f kube/app/ingress-internal.yml
-  $kd -f kube/redis -f kube/app/deployment.yml
+  $kd -f kube/redis -f kube/file-vault 
+  $kd -f kube/app/deployment.yml
   $kd -f kube/autoscale/hpa-paf.yml
   $kd -f kube/ims-resolver
 elif [[ ${KUBE_NAMESPACE} == ${PROD_ENV} ]]; then
